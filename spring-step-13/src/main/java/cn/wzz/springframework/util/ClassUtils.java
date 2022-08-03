73bd30e521f9b321f5ac3f2384c40b91c1f693d2
wzz <2439496609@qq.com>
2022-08-03 13:44:52 +0800

    13: Complete the registration of bean objects through annotation configuration and automatic package scanning

--- spring-step-13/src/main/java/cn/wzz/springframework/util/ClassUtils.java
+++ spring-step-13/src/main/java/cn/wzz/springframework/util/ClassUtils.java
@@ -1,5 +1,11 @@
 package cn.wzz.springframework.util;
 
+import java.io.File;
+import java.lang.annotation.Annotation;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ClassUtils {
 
     public static ClassLoader getDefaultClassLoader() {
@@ -31,5 +37,37 @@ public class ClassUtils {
      */
     public static boolean isCglibProxyClassName(String className) {
         return (className != null && className.contains("$$"));
+    }
+
+    public static Set<Class<?>> scanPackageByAnnotation(String basePackage, Class<?> annotationClass) throws ClassNotFoundException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        URL resource = classLoader.getResource("");
+        String basePath = resource.getPath() + basePackage.replace('.', File.separatorChar);
+        File file = new File(basePath);
+
+        return doScanPackageByAnnotation(file, annotationClass);
+    }
+
+    private static Set<Class<?>> doScanPackageByAnnotation(File file, Class<?> annotationClass) throws ClassNotFoundException {
+        Set<Class<?>> result = new HashSet<>();
+        File[] subFiles = file.listFiles();
+
+        for(File subFile: subFiles){
+            if(subFile.isDirectory()){
+                result.addAll(doScanPackageByAnnotation(subFile, annotationClass));
+            }else{
+                String filePath = subFile.getAbsolutePath();
+                int startIdx = filePath.lastIndexOf("classes\\");
+                int endIdx = filePath.lastIndexOf(".class");
+                if(startIdx != -1 && endIdx != -1 && startIdx < endIdx){
+                    String className = filePath.substring(startIdx + 8, endIdx).replace(File.separatorChar, '.');
+                    Class<?> clazz = Class.forName(className);
+
+                    if(clazz.isAnnotationPresent((Class<? extends Annotation>) annotationClass))
+                        result.add(clazz);
+                }
+            }
+        }
+        return result;
     }
 }
