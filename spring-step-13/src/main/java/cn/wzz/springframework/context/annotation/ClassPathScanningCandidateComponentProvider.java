448dd999e9e7e8f07f2d3fe36bbf64a346e9317d
wzz <2439496609@qq.com>
2022-08-03 13:44:19 +0800

    13: Complete the registration of bean objects through annotation configuration and automatic package scanning

--- spring-step-13/src/main/java/cn/wzz/springframework/context/annotation/ClassPathScanningCandidateComponentProvider.java
+++ spring-step-13/src/main/java/cn/wzz/springframework/context/annotation/ClassPathScanningCandidateComponentProvider.java
@@ -1,7 +1,8 @@
 package cn.wzz.springframework.context.annotation;
 
-import cn.hutool.core.util.ClassUtil;
+import cn.wzz.springframework.beans.BeansException;
 import cn.wzz.springframework.beans.factory.config.BeanDefinition;
+import cn.wzz.springframework.util.ClassUtils;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -10,12 +11,17 @@ import java.util.Set;
 public class ClassPathScanningCandidateComponentProvider {
 
     public Set<BeanDefinition> findCandidateComponents(String basePackage){
-        Set<BeanDefinition> candidates = new LinkedHashSet<>();
-        // 扫描basePackage目录下所有的类, 返回注解@Component修饰的Class集合
-        Set<Class<?>> classes = ClassUtil.scanPackageByAnnotation(basePackage, Component.class);
-        for(Class<?> clazz: classes){
-            candidates.add(new BeanDefinition(clazz));
+        try {
+            Set<BeanDefinition> candidates = new LinkedHashSet<>();
+            // 扫描basePackage目录下所有的类, 返回注解@Component修饰的Class集合
+            Set<Class<?>> classes = null;
+            classes = ClassUtils.scanPackageByAnnotation(basePackage, Component.class);
+            for(Class<?> clazz: classes){
+                candidates.add(new BeanDefinition(clazz));
+            }
+            return candidates;
+        } catch (ClassNotFoundException e) {
+            throw new BeansException("Fail to findCandidateComponents", e);
         }
-        return candidates;
     }
 }
